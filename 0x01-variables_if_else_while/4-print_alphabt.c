939cb666dc2a4c9379842c1627a0b716c42892f9
Waris2-tech <waris2wise@gmail.com>
2022-04-11 14:21:49 -0700

    done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *   * main-I never thought that it would pay off
+ *     *
+ *       * Return: Always (Success)
+     */
+
+int main(void)
+
+{
+
+		char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+
+	{
+	if (c != 'e' && c != 'q')
+		{
+		putchar(c);
+		}
+	}
+		putchar('\n');
+			return (0);
+
+}
