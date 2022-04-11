b879b920f9d244c193a153146d631cd4ffda0402
Waris2-tech <waris2wise@gmail.com>
2022-04-11 13:25:27 -0700

    done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *  *main-the alphabet game
+ *   *
+ *   * Return: Always (Success)
+     */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+		putchar('\n');
+	return (0);
+
+}
