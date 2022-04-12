32153b1af9652146761c81e3da2c42af8dbd02f5
Waris2-tech <waris2wise@gmail.com>
2022-04-11 20:45:52 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *   * main-Smile in the mirror
+ *	*
+ *       * Return: Always (Success)
+	 */
+
+	int main(void)
+
+	{
+
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+
+	}
