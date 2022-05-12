8a9637164d93d1f35ebe22caae2436c3a8649b08
Waris2-tech <waris2wise@gmail.com>
2022-05-11 18:44:22 -0700

    done

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,15 +22,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 	unsigned int nl, ol, i;
 
 	dog_t *dog;
+
 	if (name == NULL || owner == NULL)
 		return (NULL);
 	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
+	if (dog == NULL);
 		return (NULL);
 	for (nl = 0; name[nl]; nl++);
 	nl++;
 	dog->name = malloc(nl * sizeof(char));
-	if (dog->name == NULL)
+	if (dog->name == NULL);
 	{
 		free(dog);
 		return (NULL);
